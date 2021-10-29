8d4a9b04c679f83e6228b9789945e89d25b91caf
Alessandro <48811968+alessandrop76@users.noreply.github.com>
2021-10-29 01:01:08 -0300

    push initial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import mascote from './mascote.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={mascote} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - To Frontend - Documentation
         </a>
       </header>
     </div>
